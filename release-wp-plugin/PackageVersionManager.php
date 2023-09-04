66bbd4d21984790b169aebbc6e8a4be6177417d9
Thor Brink <thor.brink@helsingborg.se>
2023-09-04 14:20:51 +0200

    fix: php 7.4

--- release-wp-plugin/PackageVersionManager.php
+++ release-wp-plugin/PackageVersionManager.php
@@ -1,42 +1,33 @@
 <?php
 
-// phpcs:ignore PSR1.Classes.ClassDeclaration.MissingNamespace
 class PackageVersionManager
 {
-    private const PACKAGE_JSON_FILE  = 'package.json';
+    private const PACKAGE_JSON_FILE = 'package.json';
     private const COMPOSER_JSON_FILE = 'composer.json';
+
     private string $version;
     private ?string $wordPressPluginFile;
 
     public function __construct(string $version)
     {
-        $this->version             = $version;
+        $this->version = $version;
         $this->wordPressPluginFile = $this->getWordPressPluginFile();
     }
 
-    public function updatePackageFiles()
+    public function updateVersions()
     {
-        if ($this->isPackageJsonFilePresent()) {
-            $this->updatePackageJsonVersion($this->version);
-        }
-
+        $this->updatePackageJsonVersion($this->version);
         if ($this->isComposerJsonFilePresent()) {
             $this->updateComposerJsonVersion($this->version);
         }
-
-        if ($this->wordPressPluginFile !== null) {
+        if ($this->wordPressPluginFile) {
             $this->updateWordPressPluginVersion($this->version);
         }
     }
 
-    private function isPackageJsonFilePresent(): bool
+    private function updatePackageJsonVersion(string $version): void
     {
-        return file_exists('package.json');
-    }
-
-    private function updatePackageJsonVersion(string $version)
-    {
-        $packageJson            = json_decode(file_get_contents(self::PACKAGE_JSON_FILE), true);
+        $packageJson = json_decode(file_get_contents(self::PACKAGE_JSON_FILE), true);
         $packageJson['version'] = $version;
         file_put_contents(self::PACKAGE_JSON_FILE, json_encode($packageJson, JSON_PRETTY_PRINT));
         exec('npm update --package-lock-only');
@@ -45,24 +36,24 @@ class PackageVersionManager
 
     private function isComposerJsonFilePresent(): bool
     {
-        return file_exists('composer.json');
+        return file_exists(self::COMPOSER_JSON_FILE);
     }
 
-    private function updateComposerJsonVersion(string $version)
+    private function updateComposerJsonVersion(string $version): void
     {
-        $composerJson            = json_decode(file_get_contents(self::COMPOSER_JSON_FILE), true);
+        $composerJson = json_decode(file_get_contents(self::COMPOSER_JSON_FILE), true);
         $composerJson['version'] = $version;
         file_put_contents(self::COMPOSER_JSON_FILE, json_encode($composerJson, JSON_PRETTY_PRINT));
         exec('composer update --lock');
         print "Updated composer.json version to $this->version\n";
     }
 
-    private function getWordPressPluginFile(): string|null
+    private function getWordPressPluginFile(): ?string
     {
         $pluginFiles = glob('./*.php');
         foreach ($pluginFiles as $pluginFile) {
             $pluginFileContents = file_get_contents($pluginFile);
-            $thisFile           = "./" . basename(__FILE__);
+            $thisFile = "./" . basename(__FILE__);
             if (preg_match('/\* Plugin Name: (.*)/', $pluginFileContents, $matches) && $pluginFile !== $thisFile) {
                 return $pluginFile;
             }
@@ -71,7 +62,7 @@ class PackageVersionManager
         return null;
     }
 
-    private function updateWordPressPluginVersion(string $version)
+    private function updateWordPressPluginVersion(string $version): void
     {
         $pluginFileContents = file_get_contents($this->wordPressPluginFile);
         $pluginFileContents = preg_replace('/Version: (.*)/', "Version: $version", $pluginFileContents);
